ac55f1fff005b490905ee3560140c9c98a7399f1
AVINASHGOPI110 <gopiavinash110@gmail.com>
2026-04-26 10:52:28 +0530

    Update analyze.js

--- api/analyze.js
+++ api/analyze.js
@@ -14,7 +14,7 @@ module.exports = async function handler(req, res) {
     const cleanImage = image.replace(/^data:image\/\w+;base64,/, "");
 
     const response = await fetch(
-      `https://generativelanguage.googleapis.com/v1/models/gemini-1.5-flash:generateContent?key=${API_KEY}`,
+      `https://generativelanguage.googleapis.com/v1/models/gemini-2.5-flash:generateContent?key=${API_KEY}`,
       {
         method: "POST",
         headers: {
